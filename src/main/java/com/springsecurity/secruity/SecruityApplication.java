6c86c94f3fcd1cbba70b0b047808189ccee8b150
suleimin <suleimin@gmail.com>
2024-09-07 23:14:24 +0900

    basic for springsecurity

---
+++ src/main/java/com/springsecurity/secruity/SecruityApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.secruity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecruityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecruityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/secruity/SecruityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.secruity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecruityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
